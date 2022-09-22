560ef534d071ee7a1f3dae916ed0a8cee91d74a5
ana paula <ana.pmonteiro@mercadolivre.com>
2022-09-22 19:53:46 -0300

    refactor: melhoria do codigo do exercicio de matriz

--- src/atividade/AtividadeProposta.java
+++ src/atividade/AtividadeProposta.java
@@ -17,59 +17,41 @@ public class AtividadeProposta {
 
     int temperaturas [][] = new int [10][2];
         temperaturas [0][0] = -2;
+        temperaturas [0][1] = 33;
         temperaturas [1][0] = -3;
-        temperaturas [2][0] = -8;
-        temperaturas [3][0] = 4;
-        temperaturas [4][0] = 6;
-        temperaturas [5][0] = 5;
-        temperaturas [6][0] = 0;
-        temperaturas [7][0] = -7;
-        temperaturas [8][0] = -1;
-        temperaturas [9][0] = -10;
-
-        temperaturas [0][0] = 33;
         temperaturas [1][1] = 32;
+        temperaturas [2][0] = -8;
         temperaturas [2][1] = 27;
+        temperaturas [3][0] = 4;
         temperaturas [3][1] = 37;
+        temperaturas [4][0] = 6;
         temperaturas [4][1] = 42;
+        temperaturas [5][0] = 5;
         temperaturas [5][1] = 43;
+        temperaturas [6][0] = 0;
         temperaturas [6][1] = 39;
+        temperaturas [7][0] = -7;
         temperaturas [7][1] = 26;
+        temperaturas [8][0] = -1;
         temperaturas [8][1] = 31;
+        temperaturas [9][0] = -10;
         temperaturas [9][1] = 35;
 
-        int maiorTemperatura = temperaturas[0][0];
-        int menorTemperatura = temperaturas[0][0];
-        String cidadeMaiorTemperatura = cidade[0];
-        String cidadeMenorTemperatura = cidade[0];
+        int cidadeMaiorTemperatura = 0;
+        int cidadeMenorTemperatura = 0;
 
-        for(int i = 0; i < 10; i++){
-            for (int j = 0; j < 2; j++) {
-                if(menorTemperatura > temperaturas[i][j] ){
-                    menorTemperatura = temperaturas[i][j];
-                    cidadeMenorTemperatura = cidade[i];
-                }
-                if(maiorTemperatura < temperaturas[i][j]){
-                    maiorTemperatura = temperaturas[i][j];
-                    cidadeMaiorTemperatura = cidade[i];
-                }
+
+        for(int i = 0; i < temperaturas.length; i++){
+            if(temperaturas[i][0] < temperaturas[cidadeMenorTemperatura][0]){
+                cidadeMenorTemperatura = i;
             }
+            if(temperaturas[i][1] > temperaturas[cidadeMaiorTemperatura][1]){
+                cidadeMaiorTemperatura = i;
+            }
+
         }
-        System.out.println(cidadeMenorTemperatura + " com " + menorTemperatura + " graus");
-        System.out.println(cidadeMaiorTemperatura + " com " + maiorTemperatura + " graus");
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println(cidade[cidadeMenorTemperatura] + " teve a temperatura minima de " + temperaturas[cidadeMenorTemperatura][0] + " graus");
+        System.out.println(cidade[cidadeMaiorTemperatura] + " teve a temperatura maxima de  " + temperaturas[cidadeMaiorTemperatura][1] + " graus");
 
     }
 }
